680cf1485164f4e6108319074ab0d64dd21d9359
Jesus_0414 <jesus140400@gmail.com>
2019-10-31 12:50:46 -0700

    Bubble. <3

--- Ordenamiento/MainWindow.xaml.cs
+++ Ordenamiento/MainWindow.xaml.cs
@@ -39,30 +39,45 @@ namespace Ordenamiento
         private void btnOrdenar_Click(object sender, RoutedEventArgs e)
         {
             /*int i;int j;for(i=0; i> miLista.Count; i++){for (j = 1; j > miLista.Count; j++){var temp = miLista[j];miLista[j] = miLista[j+1];miLista[j+1] = temp;}}*/
-            int gap, temp, i, j;
+            int gap, temp, i;
             gap = miLista.Count / 2;
 
             while (gap > 0)
             {
                 for(i = 0; i < miLista.Count; i++)
                 {
-                    if (miLista[gap + i] < miLista.Count)
+                    if (gap + i < miLista.Count && miLista[i] > miLista[gap + i])
                     {
-                        if (miLista[i] > miLista[gap + i])
-                        {
-                            temp = miLista[i];
-                            miLista[i] = miLista[gap + i];
-                            miLista[gap + i] = temp;
-                        }
+                        temp = miLista[i];
+                        miLista[i] = miLista[gap + i];
+                        miLista[gap + i] = temp;
                     }
-                    else break;
-                    
-                }
-                
 
+                }
                 gap--;
             }
 
+        }
+
+        private void btnBubble_Click(object sender, RoutedEventArgs e)
+        {
+            bool intercambio = false;
+            do
+            {
+                intercambio = false;
+                for (int i = 0; i < miLista.Count - 1; i++)
+                {
+                    if (miLista[i] > miLista[i + 1])
+                    {
+                        int temp = miLista[i];
+                        miLista[i] = miLista[i + 1];
+                        miLista[i + 1] = temp;
+                        intercambio = true;
+                    }
+                }
+            /*int i, j, temp;for (i = 0; i < miLista.Count; i++){for (j = 0; j < i + 1; j++){if(j + 1 < miLista.Count && miLista[j] > miLista[j + 1]){temp = miLista[j];miLista[j] = miLista[j + 1];miLista[j + 1] = temp;}}}intercambio = true;break;*/
+            } while (intercambio);
+           
         }
     }
 }
